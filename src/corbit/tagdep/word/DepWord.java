eb71a2eb88a274cd453762dc55075f1ff0419633
Atefe Pakzad <atefehpakzad@gmail.com>
2014-07-07 18:39:24 +0430

    Update DepWord.java

--- src/corbit/tagdep/word/DepWord.java
+++ src/corbit/tagdep/word/DepWord.java
@@ -35,19 +35,19 @@ public class DepWord extends Word {
     public int head;
     public DepTreeSentence sent;
 
-    public DepWord(DepTreeSentence sent, int index, String form, String pos, int head) {
-        super(form, pos);
+    public DepWord(DepTreeSentence sent, int index, String form, String pos,String lemm, int head) {
+        super(form, pos, lemm);
         this.sent = sent;
         this.index = index;
         this.head = head;
     }
 
     public DepWord(DepWord dw) {
-        this(dw.sent, dw.index, dw.form, dw.pos, dw.head);
+        this(dw.sent, dw.index, dw.form, dw.pos,dw.lemm, dw.head);
     }
 
     public DepWord() {
-        this(null, -1, null, null, -1);
+        this(null, -1, null, null,null, -1);
     }
 
     @Override
